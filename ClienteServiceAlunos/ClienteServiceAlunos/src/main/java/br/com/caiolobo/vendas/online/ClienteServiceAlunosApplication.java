6e058e12900a84a01ff85fa1ba78eecaa0c24202
Caio Lobo <caioeduardolobo@gmail.com>
2023-02-08 11:16:10 -0300

    create configuration and crete Cliente domain

---
+++ ClienteServiceAlunos/ClienteServiceAlunos/src/main/java/br/com/caiolobo/vendas/online/ClienteServiceAlunosApplication.java
@@ -0,0 +1,14 @@
+package br.com.caiolobo.vendas.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ClienteServiceAlunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteServiceAlunosApplication.class, args);
+	}
+
+}
---
+++ ClienteServiceAlunos/ClienteServiceAlunos/src/main/java/br/com/caiolobo/vendas/online/domain/Cliente.java
@@ -0,0 +1,5 @@
+package br.com.caiolobo.vendas.online.domain;
+
+public class Cliente {
+
+}

54e806dfdbfed0b687551e2fe01000ef0df95fb8
Caio Lobo <caioeduardolobo@gmail.com>
2023-02-08 11:52:34 -0300

    create IClienteRepository

---
+++ ClienteServiceAlunos/ClienteServiceAlunos/src/main/java/br/com/caiolobo/vendas/online/repository/IClienteRepository.java
@@ -0,0 +1,14 @@
+package br.com.caiolobo.vendas.online.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.caiolobo.vendas.online.domain.Cliente;
+
+@Repository
+public interface IClienteRepository extends MongoRepository<Cliente, String>{
+	
+	Optional<Cliente> findByCpf(Long cpf);
+}

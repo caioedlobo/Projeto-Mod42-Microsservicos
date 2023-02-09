fa689366e466f910f709491dfc6e0e972bfcab5a
Caio Lobo <caioeduardolobo@gmail.com>
2023-02-09 18:19:06 -0300

    create config for VendaService

---
+++ VendaServiceAlunos/VendaServiceAlunos/src/main/java/br/com/caiolobo/vendas/online/config/ValidatorConfig.java
@@ -0,0 +1,20 @@
+package br.com.caiolobo.vendas.online.config;
+
+import jakarta.validation.Validator;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+public class ValidatorConfig {
+ ResourceBundleMessageSource messageSource;
+
+	@Bean
+    public Validator validatorFactory () {
+		LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+//	    bean.setValidationMessageSource(messageSource);
+	    return bean;
+    }
+}
